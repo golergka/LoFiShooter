6ec9c86c7e8d7ee2b211c978e9cb404cf910b2de
Max Yankov <golergka@gmail.com>
2013-03-31 17:10:42 +0400

    Fixed targeting. Now the invisible cursorPlane is created and used for ray cast from cursor.

--- Assets/Scripts/Characters/Player/PlayerMovement.cs
+++ Assets/Scripts/Characters/Player/PlayerMovement.cs
@@ -3,13 +3,28 @@ using System.Collections;
 
 public class PlayerMovement : BasicBehavior {
 
-	const string AXIS_HORIZONTAL = "Horizontal";
-	const string AXIS_VERTICAL   = "Vertical";
+	const string AXIS_HORIZONTAL    = "Horizontal";
+	const string AXIS_VERTICAL      = "Vertical";
+
+	const int    LAYER_CURSOR_PLANE = 9;
 
 	[ComponentField]
 	MovementController movementController;
 
 	public float speed = 1f;
+
+	protected override void Awake() {
+		base.Awake();
+
+		GameObject cursorPlane = new GameObject("cursorPlane");
+		cursorPlane.transform.localScale = new Vector3(100000f,0.00001f,100000f);
+		// just big enough numbers that not gonna be used anywhere else, no reason to make them constants
+		cursorPlane.transform.position = transform.position;
+		cursorPlane.transform.parent = transform;
+		cursorPlane.layer = 9; // CursorPlane
+		cursorPlane.AddComponent<BoxCollider>();
+
+	}
 
 	public override void OnGameReset() { }
 	
@@ -26,8 +41,9 @@ public class PlayerMovement : BasicBehavior {
 
 		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 		RaycastHit hit;
+		int layerMask = 1 << LAYER_CURSOR_PLANE;
 		
-		if (Physics.Raycast(ray, out hit)) {
+		if (Physics.Raycast(ray, out hit, layerMask)) {
 			
 			Vector3 lookTarget = hit.point;
 			lookTarget.y = transform.position.y;
